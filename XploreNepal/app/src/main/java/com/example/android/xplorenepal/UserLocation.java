4bb2366479d76aaa6019d9a1e369ed5a30632f1f
Junth Basnet <25685098+Junth19@users.noreply.github.com>
2018-06-12 16:27:37 +0545

    Tourist Guide

---
+++ XploreNepal/app/src/main/java/com/example/android/xplorenepal/UserLocation.java
@@ -0,0 +1,15 @@
+package com.example.android.xplorenepal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_location);
+
+
+    }
+}
